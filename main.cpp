e8c25842c03b3482e45e20ed418c24008e47fddf
AngelDCR4 <149647380+AngelDCR4@users.noreply.github.com>
2024-11-10 20:36:42 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@ using namespace std;
 
 int main() {
     Paquete ejemplo;
-    vector<Paquete> paquetes = ejemplo.ejemplos();
+    vector<Paquete> paquetes;
+    ejemplo.ejemplos(paquetes);  // Cargar los paquetes desde el archivo CSV
+
     Sorts<float> sorts;
     Arbol arbol;
 
@@ -21,8 +23,9 @@ int main() {
         cout << "1 -> Ver pedidos actuales\n";
         cout << "2 -> Agregar pedidos\n";
         cout << "3 -> Ver distancias mas cortas (Merge Sort)\n";
-        cout << "4 -> Hacer arbol e imprimir in-order\n";
-        cout << "5 -> Salir\n";
+        cout << "4 -> Ver distancias ordenadas segun el ABB (In-order)\n";
+        cout << "5 -> Guardar pedidos en archivo\n";
+        cout << "6 -> Salir\n";
         cout << "Seleccione una opcion: ";
 
         int opcion;
@@ -43,7 +46,8 @@ int main() {
             sorts.ordenaMerge(distancias);
             cout << "Distancias ordenadas (Merge Sort):\n";
             for (int i = 0; i < distancias.size(); i++) {
-                cout << fixed << setprecision(2) << distancias[i] << " -> "; //REDONDEAR A DOS DECIMALES
+                cout << "Paquete " << i + 1 << " - " << paquetes[i].get_nombreLugar() << " -> "
+                << fixed << setprecision(2) << distancias[i] << endl;
             }
             cout << "\n";
         
@@ -51,10 +55,16 @@ int main() {
 
             vector<float> distancias = ejemplo.calcdist(paquetes);
             arbol.construir_arbol(distancias);
-            cout << "Arbol in-order:\n";
+            cout << "\nCreando arbol binario de busqueda...\n";
+            cout << "Ordenando distancias en recorrido in-order...\n";
+            cout << "Distancias desplegadas de menor a mayor:\n";
             arbol.imprimir_inorder();
         
         } else if (opcion == 5) {
+
+            ejemplo.guardar_en_archivo(paquetes);
+        
+        } else if (opcion == 6) {
 
             continuar = false;
             cout << "Saliendo del sistema...\n";
